962ba1135c5811088b22cfe2d4cca2d53c12c0ac
Linming-XHL <LinmingXHL@qq.com>
2026-04-06 12:05:03 +0800

    feat: 状态栏保留留白并设置颜色为#1F2233，重新添加版本更新检测，修改Android版本号

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,9 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        // 自动获取版本号
+        versionCode = 1
+        versionName = "1.0.13"
     }
 
     buildTypes {
